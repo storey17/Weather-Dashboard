a9deced7bdffa2113a2a836a9f5feb207501421b
Storey Sheldon <storey.e.sheldon@gmail.com>
2020-02-03 18:40:21 -0600

    fixed js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-$(".btn").on("click", function() {
-    event.preventDefault();
-    var $cityName = $("#city-input").val();
-    console.log($cityName);
-    localStorage.setItem("cityname", $cityName);
-    var liElement = $("<li>");
-    var appendedLi = liElement.prependTo($("#search1"));
-    var retrievedLocalCity = localStorage.getItem($cityName);
-    appendedLi.text(retrievedLocalCity);
-})
+    // WHEN I search for a city
+    // THEN I am presented with current and future conditions for that city and that city is added to the search history
+    $(".btn").on("click", function () {
+        event.preventDefault();
+        var $cityName = $("#city-input").val();
+        console.log($cityName);
+        localStorage.setItem("cityname", $cityName);
+        var liElement = $("<li>");
+        var appendedLi = liElement.prependTo($("#search1"));
+        var retrievedLocalCity = localStorage.getItem($cityName);
+        appendedLi.text(retrievedLocalCity);
 
-var $cityName;
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $cityName + "&appid=094a2720fa99404e218aff094ec83870";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=094a2720fa99404e218aff094ec83870";
+        var $cityName;
+        // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $cityName + "&appid=094a2720fa99404e218aff094ec83870";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=094a2720fa99404e218aff094ec83870";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  $(".card-title").text($cityName);
-  $("#temp").text("Temperature: " + response.main.temp);
-  $("#humidity").text("Humidity: " + response.main.humidity);
-  $("#wind").text("Wind Speed: " + response.wind.speed);  
-  $("#uv").text("UV Index: " + response.main.uv);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            $(".card-title").text($cityName);
+            $("#temp").text("Temperature: " + response.main.temp);
+            $("#humidity").text("Humidity: " + response.main.humidity);
+            $("#wind").text("Wind Speed: " + response.wind.speed);
+            $("#uv").text("UV Index: " + response.main.uv);
 
-});
+        })
+    });
+
 
 });
 
